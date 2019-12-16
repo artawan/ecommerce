2d51d6701f9a85bf103b2e2733150718762cf77c
artawan <gsumiartawan@gmail.com>
2019-12-16 17:07:38 +0800

    Create Home Page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Homepage from './component/Homepage/homepage.component'
+
 class App extends Component {
   render() {
-    return <div className='App'>Hello World</div>;
+    return <Homepage/>;
   }
 }
 
